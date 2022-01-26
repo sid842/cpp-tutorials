48d5075511082dbd9cf38635205cd63d9a10c871
Siddharth <sidbhasin82@gmail.com>
2022-01-26 00:38:10 +0000

    Add true and false type struct to reduce duplication

--- templates-mp/main.cpp
+++ templates-mp/main.cpp
@@ -39,53 +39,47 @@ public:
 };
 
 
-/* Type traits for container*/
-template<typename T>
-struct is_type_container {
+/* True type and False type*/
+struct true_type {
+    static const bool value = true;
+};
+
+struct false_type {
     static const bool value = false;
 };
+
+
+/* Type traits for container*/
 template<typename T>
-struct is_type_container<std::vector<T>> {
-    static const bool value = true;
-};
+struct is_type_container : false_type {};
+
 template<typename T>
-struct is_type_container<std::set<T>> {
-    static const bool value = true;
-};
+struct is_type_container<std::vector<T>> : true_type {};
+template<typename T>
+struct is_type_container<std::set<T>> : true_type {};
 
 
 /* Type traits for user defined types with toString method */
 template<typename T>
-struct has_to_string {
-    static const bool value = false;
-};
+struct has_to_string : false_type {};
+
 template<>
-struct has_to_string<House> {
-    static const bool value = true;
-};
+struct has_to_string<House> : true_type {};
 template<>
-struct has_to_string<Person> {
-    static const bool value = true;
-};
+struct has_to_string<Person> : true_type {};
 template<>
-struct has_to_string<Animal> {
-    static const bool value = true;
-};
+struct has_to_string<Animal> : true_type {};
 
 
 /* Type traits for built in types */
 template<typename T>
-struct is_built_in_type {
-    static const bool value = false;
-};
+struct is_built_in_type :false_type {};
+
 template<>
-struct is_built_in_type<int> {
-    static const bool value = true;
-};
+struct is_built_in_type<int> : true_type {};
 template<>
-struct is_built_in_type<float> {
-    static const bool value = true;
-};
+struct is_built_in_type<float> : true_type {};
+
 
 /* ToString definitions for each type */
 
